b732f0bf2fd2d59f1bcd44e862f0510797992928
Ygrik2003 <59407669+Ygrik2003@users.noreply.github.com>
2022-11-05 20:20:09 +0300

    Update cellbutton.cpp

--- cellbutton.cpp
+++ cellbutton.cpp
@@ -3,39 +3,7 @@
 
 CellButton::CellButton(Type type, QPoint position, Color color, QWidget *parent) : QPushButton(parent)
 {
-    this->setCheckable(true);
-    this->position = position;
-    this->type = type;
-    this->color = color;
-    switch (type)
-    {
-    case Colored:
-        switch (color)
-        {
-        case Red:
-            this->setStyleSheet("background-color: red");
-            break;
-        case Green:
-            this->setStyleSheet("background-color: green");
-            break;
-        case Blue:
-            this->setStyleSheet("background-color: blue;");
-            break;
-        case None:
-            this->setStyleSheet("background-color: black");
-            break;
-        }
-
-        break;
-    case Passed:
-        this->setStyleSheet("background-color: rgba(0,0,0,0)");
-        break;
-    case Type::Blocked:
-        this->setStyleSheet("background-color: black");
-
-        this->setDisabled(true);
-        break;
-    }
+    setCellParams(type, position, color)
 }
 
 void CellButton::setCellParams(Type type, QPoint position, Color color)
